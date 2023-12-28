91f603652044a2fb3fdd2fc2992d98f5f0ee4f33
fansg666 <2680315178@qq.com>
2023-12-28 12:07:28 +0800

    完结

--- DataStruct/main.go
+++ DataStruct/main.go
@@ -3,82 +3,74 @@ package main
 import (
 	"DataStruct/man"
 	"DataStruct/stack"
-	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 )
 
 const (
-	MAXVEX   = 30
+	MAXVex   = 30
 	INFINITY = 32768
 )
 
-type AdjMatrix struct {
-	arcs   [MAXVEX][MAXVEX]int
-	vex    [MAXVEX]string
-	info   [MAXVEX]string
-	vexnum int
-	arcnum int
+type CloseEdge struct {
+	adjVex  int
+	lowcost int
 }
 
-var visit [MAXVEX]int
+type PrimeTreeNode struct {
+	Vex1     string
+	Vex2     string
+	distance int
+}
 
-//func WriteFileAdjMatrix(G *AdjMatrix) {
-//	// 实现写入邻接矩阵到文件的逻辑
-//	fmt.Println("Writing Adjacency Matrix to file...")
-//}
-//
-//func delOldAddress(G *AdjMatrix) {
-//	// 实现删除旧地点的逻辑
-//	fmt.Println("Deleting old addresses...")
-//}
-//
-//func delOldPath(G *AdjMatrix) {
-//	// 实现删除指定路线的逻辑
-//	fmt.Println("Deleting specified paths...")
-//}
-//
-//func addNewAddressPath(G *AdjMatrix) {
-//	// 实现增加新地点和路径的逻辑
-//	fmt.Println("Adding new addresses and paths...")
-//}
-//
-//func displayAddressInfo(G *AdjMatrix) {
-//	// 实现显示指定地点信息的逻辑
-//	fmt.Println("Displaying address information...")
-//}
-//
-//func displaySimplePath(G *AdjMatrix) {
-//	// 实现显示指定两地简单路径的逻辑
-//	fmt.Println("Displaying simple path...")
-//}
-//
-//func displayShortestPath(G *AdjMatrix) {
-//	// 实现显示指定两地最短路径的逻辑
-//	fmt.Println("Displaying shortest path...")
-//}
+type PrimeTree struct {
+	Node   PrimeTreeNode
+	Lchild *PrimeTree
+	Rchild *PrimeTree
+}
+
+type AdjMatrix struct {
+	Arcs   [MAXVex][MAXVex]int `json:"Arcs"`   //邻接矩阵
+	Vex    [MAXVex]string      `json:"Vex"`    //顶点
+	Info   [MAXVex]string      `json:"Info"`   //描述信息
+	Vexnum int                 `json:"Vexnum"` //顶点数量
+	Arcnum int                 `json:"Arcnum"` //边的数量
+}
+
+var visit [MAXVex]int
 
 func getVexNo(G *AdjMatrix, str string) int {
-	for i := 1; i <= G.vexnum; i++ {
-		if G.vex[i] == str {
+	for i := 1; i <= G.Vexnum; i++ {
+		if G.Vex[i] == str {
 			return i
 		}
 	}
 	return -1
-}
+} //返回顶点的下标
 
 func Create(G *AdjMatrix) {
-	var distance, vex1, vex2, arcnum int
+	var distance, Vex1, Vex2, Arcnum int
 
 	fmt.Println("请输入地点的个数:")
-	fmt.Scanln(&G.vexnum)
+	fmt.Scanln(&G.Vexnum)
 
-	for i := 1; i <= G.vexnum; i++ {
+	for i := 1; i <= G.Vexnum; i++ {
 		fmt.Printf("第%d个地点:", i)
-		fmt.Scanln(&G.vex[i])
+		fmt.Scanln(&G.Vex[i])
 		fmt.Println("请输入该地点的介绍：")
-		fmt.Scanln(&G.info[i])
+		fmt.Scanln(&G.Info[i])
+	}
+	for i := 1; i <= G.Vexnum; i++ {
+		for j := 1; j <= G.Vexnum; j++ {
+			if i == j {
+				G.Arcs[i][j] = 0
+			} else {
+				G.Arcs[i][j] = INFINITY // 设置为一个足够大的数值，表示无穷大
+			}
+		}
 	}
 
 	fmt.Println("请输入所有路线,比如宿舍 食堂代表宿舍到食堂的路线，输入0 0结束输入：")
@@ -92,10 +84,10 @@ func Create(G *AdjMatrix) {
 			break
 		}
 
-		vex1 = getVexNo(G, add1)
-		vex2 = getVexNo(G, add2)
+		Vex1 = getVexNo(G, add1)
+		Vex2 = getVexNo(G, add2)
 
-		if vex1 == -1 || vex2 == -1 {
+		if Vex1 == -1 || Vex2 == -1 {
 			fmt.Println("此地点不存在！请重新输入！！！")
 			continue
 		}
@@ -103,95 +95,91 @@ func Create(G *AdjMatrix) {
 		fmt.Printf("请输入%s到%s的距离：", add1, add2)
 		fmt.Scanln(&distance)
 
-		G.arcs[vex1][vex2] = distance
-		G.arcs[vex2][vex1] = distance
-		arcnum++
+		G.Arcs[Vex1][Vex2] = distance
+		G.Arcs[Vex2][Vex1] = distance
+		Arcnum++
 	}
-
-	G.arcnum = arcnum
-	WriteFileAdjMatrix(G)
+	G.Arcnum = Arcnum
+	//fmt.Println(G)
+	WriteFileAdjMatrix("AdjMatrix.txt", G)
 }
-func printAdjMatrix(G *AdjMatrix) {
-	fmt.Println("\nAdjMatrix:")
-	fmt.Print("     ")
-	for i := 1; i <= G.vexnum; i++ {
-		fmt.Printf("%5s", G.vex[i])
+func printTree(root *PrimeTree) {
+	if root == nil {
+		return
 	}
-	fmt.Println()
-	for i := 1; i <= G.vexnum; i++ {
-		fmt.Printf("%-15s", G.vex[i])
-		for j := 1; j <= G.vexnum; j++ {
-			if G.arcs[i][j] == INFINITY {
-				fmt.Print(" ----")
-			} else {
-				fmt.Printf("%5d", G.arcs[i][j])
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Scanln()
+	fmt.Println(root.Node.Vex1, "----------", root.Node.distance, "------------", root.Node.Vex2)
+	printTree(root.Rchild)
+	printTree(root.Lchild)
 }
-
-func prime(G *AdjMatrix, start int) {
-	type CloseEdge struct {
-		adjvex  int
-		lowcost int
-	}
-
-	type PrimeTree struct {
-		vex1     string
-		vex2     string
-		distance int
-	}
-	closedge := make([]CloseEdge, MAXVEX)
-	primeTree := make([]PrimeTree, MAXVEX)
-
-	for i := 1; i <= G.vexnum; i++ {
+func prime(G *AdjMatrix, start int) *PrimeTree {
+	closedge := make([]CloseEdge, MAXVex)
+	// 初始化 closedge 数组
+	for i := 1; i <= G.Vexnum; i++ {
 		if i != start {
-			closedge[i].adjvex = start
-			closedge[i].lowcost = G.arcs[start][i]
+			closedge[i].adjVex = start
+			closedge[i].lowcost = G.Arcs[start][i]
 		}
 	}
-
-	for e := 1; e <= G.vexnum; e++ {
+	// 根节点
+	root := &PrimeTree{}
+	var buildTree func(parent *PrimeTree, e int)
+	buildTree = func(parent *PrimeTree, e int) {
 		min := INFINITY
-		m := 0
-
-		for k := 1; k <= G.vexnum; k++ {
+		m := 0 // 最近的顶点
+		// 在 closedge 数组中找到最小权值的边
+		for k := 1; k <= G.Vexnum; k++ {
 			if closedge[k].lowcost != 0 && closedge[k].lowcost < min {
 				m = k
 				min = closedge[k].lowcost
 			}
 		}
-
-		primeTree[e].vex1 = G.vex[closedge[m].adjvex]
-		primeTree[e].vex2 = G.vex[m]
-		primeTree[e].distance = min
-
-		closedge[m].lowcost = 0
-
-		for i := 1; i <= G.vexnum; i++ {
-			if i != m && G.arcs[m][i] < closedge[i].lowcost {
-				closedge[i].lowcost = G.arcs[m][i]
-				closedge[i].adjvex = m
+		// 将找到的边添加到最小生成树中
+		node := PrimeTreeNode{
+			Vex1:     G.Vex[closedge[m].adjVex],
+			Vex2:     G.Vex[m],
+			distance: min,
+		}
+		current := &PrimeTree{Node: node}
+		if parent != nil {
+			if parent.Lchild == nil {
+				parent.Lchild = current
+			} else {
+				parent.Rchild = current
 			}
+		} else {
+			parent = current
+		}
+		closedge[m].lowcost = 0
+		for i := 1; i <= G.Vexnum; i++ {
+			if i != m && G.Arcs[m][i] < closedge[i].lowcost {
+				closedge[i].lowcost = G.Arcs[m][i]
+				closedge[i].adjVex = m
+			}
+		}
+		if e < G.Vexnum {
+			buildTree(current, e+1)
 		}
 	}
 
+	// 从根节点开始构建最小生成树
+	buildTree(root, 1)
+
+	// 打印最小生成树的结果
 	fmt.Println("最佳布网路线如下：")
-	for i := 1; i <= G.vexnum-1; i++ {
-		fmt.Printf("%s-->%s 距离：%d\n", primeTree[i].vex1, primeTree[i].vex2, primeTree[i].distance)
-	}
+	printTree(root)
+
+	return root
 }
+
 func printPath(G *AdjMatrix) {
 	clearCommand := exec.Command("clear")
 	clearCommand.Stdout = os.Stdout
 	clearCommand.Run()
 
-	for i := 1; i <= G.vexnum; i++ {
-		for j := 1; j <= G.vexnum; j++ {
-			if G.arcs[i][j] != INFINITY {
-				fmt.Printf("%s-->%s 距离：%d     ", G.vex[i], G.vex[j], G.arcs[i][j])
+	for i := 1; i <= G.Vexnum; i++ {
+		for j := 1; j <= G.Vexnum; j++ {
+			if G.Arcs[i][j] != INFINITY {
+				fmt.Printf("%s-->%s 距离：%d     ", G.Vex[i], G.Vex[j], G.Arcs[i][j])
 			}
 		}
 		fmt.Println()
@@ -201,39 +189,36 @@ func printPath(G *AdjMatrix) {
 	fmt.Scanln()
 }
 
-func ReadFileCreateAdjMatrix(G *AdjMatrix) {
-	file, err := os.Open("AdjMatrix.txt")
+func WriteFileAdjMatrix(filePath string, G *AdjMatrix) error {
+	data, err := json.MarshalIndent(G, "", "    ")
 	if err != nil {
-		fmt.Println("文件访问出错")
-		os.Exit(0)
+		return err
 	}
-	defer file.Close()
 
-	err = binary.Read(file, binary.BigEndian, G)
+	err = ioutil.WriteFile(filePath, data, 0644)
 	if err != nil {
-		fmt.Println("读取文件出错")
-		os.Exit(0)
+		return err
 	}
+
+	fmt.Printf("Adjacency Matrix written to %s\n", filePath)
+	return nil
 }
 
-func WriteFileAdjMatrix(G *AdjMatrix) {
-	file, err := os.OpenFile("AdjMatrix.txt", os.O_WRONLY|os.O_CREATE, 0644)
+func ReadFileCreateAdjMatrix(filePath string, G *AdjMatrix) error {
+	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		fmt.Println("文件访问出错，按任意键返回......")
-		fmt.Scanln()
-		fmt.Scanln()
-		return
+		return err
 	}
-	defer file.Close()
 
-	err = binary.Write(file, binary.BigEndian, G)
+	err = json.Unmarshal(data, G)
 	if err != nil {
-		fmt.Println("写入文件出错")
-		fmt.Scanln()
-		fmt.Scanln()
-		return
+		return err
 	}
+
+	fmt.Printf("Adjacency Matrix read from %s\n", filePath)
+	return nil
 }
+
 func bestNetworkPath(G *AdjMatrix) {
 	clearCommand := exec.Command("clear")
 	clearCommand.Stdout = os.Stdout
@@ -279,8 +264,8 @@ func onlyAddNewPath(G *AdjMatrix) {
 	fmt.Printf("请输入%s到%s的距离：", str1, str2)
 	fmt.Scan(&distance)
 
-	G.arcs[num1][num2] = distance
-	WriteFileAdjMatrix(G)
+	G.Arcs[num1][num2] = distance
+	WriteFileAdjMatrix("AdjMatrix.txt", G)
 
 	fmt.Println("添加/修改成功，按任意键返回......")
 	fmt.Scanln()
@@ -293,23 +278,36 @@ func menu(G *AdjMatrix) {
 		clearCommand := exec.Command("clear")
 		clearCommand.Stdout = os.Stdout
 		clearCommand.Run()
-
-		fmt.Println("\n\n\t\t\t\t校园导航图")
-		fmt.Println("\t\t\t*************************")
-		fmt.Println("\t\t\t1.创建新的路线图")
-		fmt.Println("\t\t\t2.显示指定地点的信息")
-		fmt.Println("\t\t\t3.显示指定两地的简单路径")
-		fmt.Println("\t\t\t4.显示指定两地的最短路线")
-		fmt.Println("\t\t\t5.增加新的地点和路线")
-		fmt.Println("\t\t\t6.删除旧地点")
-		fmt.Println("\t\t\t7.删除指定路线")
-		fmt.Println("\t\t\t8.显示所有的路线")
-		fmt.Println("\t\t\t9.最佳布网方案")
-		fmt.Println("\t\t\t10.显示平面图")
-		fmt.Println("\t\t\t11.添加新路线或修改路线长度")
-		fmt.Println("\t\t\t0.退出")
-		fmt.Println("\t\t\t*************************")
-		fmt.Print("\t\t\t  请选择：")
+		fmt.Println("\t\t                ┌───────────────────┐                ")
+		fmt.Println("\t\t┌──────├── 欢迎使用西邮导航系统 ──┤──────┐")
+		fmt.Println("\t\t│              └───────────────────┘              │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t│   ┌────────────┐        ┌────────────┐   │")
+		fmt.Println("\t\t│   │ 1.创建新的路线图 │        │ 2.显示指定地点的信息 │   │")
+		fmt.Println("\t\t│   └────────────┘        └────────────┘   │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t│   ┌────────────┐        ┌────────────┐   │")
+		fmt.Println("\t\t│   │ 3.显示指定两地的简单路径 │  │ 4.显示指定两地的最短路线 │   │")
+		fmt.Println("\t\t│   └────────────┘        └────────────┘   │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t│   ┌────────────┐        ┌────────────┐   │")
+		fmt.Println("\t\t│   │ 5.增加新的地点和路线 │     │ 6.删除旧地点 │   │")
+		fmt.Println("\t\t│   └────────────┘        └────────────┘   │")
+		fmt.Println("\t\t│   ┌────────────┐        ┌────────────┐   │")
+		fmt.Println("\t\t│   │ 7.删除指定路线 │  │ 8.显示所有的路线 │   │")
+		fmt.Println("\t\t│   └────────────┘        └────────────┘   │")
+		fmt.Println("\t\t│   ┌────────────┐        ┌────────────┐   │")
+		fmt.Println("\t\t│   │ 9.最佳布网方案 │  │ 10. │ 显示平面图  │")
+		fmt.Println("\t\t│   └────────────┘        └────────────┘   │")
+		fmt.Println("\t\t│   ┌────────────┐        ┌────────────┐   │")
+		fmt.Println("\t\t│   │ 11. │ 添加新路线或修改路线长度 │ 0. │ 退出 │")
+		fmt.Println("\t\t│   └────────────┘        └────────────┘   │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t│                                                                      │")
+		fmt.Println("\t\t└───────────────────────────────────┘\n\n")
+		fmt.Print("\t\t请根据你的需求选择操作：")
 		fmt.Scan(&selectOption)
 
 		switch selectOption {
@@ -343,6 +341,7 @@ func menu(G *AdjMatrix) {
 	}
 }
 func displayAddressInfo(G *AdjMatrix) {
+	//实现显示指定地点信息的逻辑
 	clearCommand := exec.Command("clear")
 	clearCommand.Stdout = os.Stdout
 	clearCommand.Run()
@@ -356,21 +355,22 @@ func displayAddressInfo(G *AdjMatrix) {
 	if num == -1 {
 		fmt.Println("该地点不存在！")
 	} else {
-		fmt.Printf("%s 简介: %s\n", G.vex[num], G.info[num])
+		fmt.Printf("%s 简介: %s\n", G.Vex[num], G.Info[num])
 	}
 
 	fmt.Scanln()
 	fmt.Scanln()
 }
 func gettopnextAdj(G *AdjMatrix, top int, st int) int {
-	for i := st + 1; i <= G.vexnum; i++ {
-		if G.arcs[top][i] != INFINITY {
+	for i := st + 1; i <= G.Vexnum; i++ {
+		if G.Arcs[top][i] != INFINITY {
 			return i
 		}
 	}
 	return 0
 }
 func displaySimplePath(G *AdjMatrix) {
+	//实现显示指定两地简单路径的逻辑
 	clearCommand := exec.Command("clear")
 	clearCommand.Stdout = os.Stdout
 	clearCommand.Run()
@@ -378,7 +378,7 @@ func displaySimplePath(G *AdjMatrix) {
 	var str1, str2 string
 	var start, end, st, top, i int
 	count := INFINITY
-	mindist := make([]int, MAXVEX)
+	mindist := make([]int, MAXVex)
 	S := new(stack.Stack)
 	S.Top = -1
 
@@ -416,9 +416,9 @@ func displaySimplePath(G *AdjMatrix) {
 						mindist[i] = S.Num[i]
 					}
 				}
-				fmt.Print(G.vex[S.Num[0]])
+				fmt.Print(G.Vex[S.Num[0]])
 				for i = 1; i <= S.Top; i++ {
-					fmt.Printf("-->%s", G.vex[S.Num[i]])
+					fmt.Printf("-->%s", G.Vex[S.Num[i]])
 				}
 				fmt.Println()
 			}
@@ -428,9 +428,9 @@ func displaySimplePath(G *AdjMatrix) {
 	fmt.Println("\n\n中转次数最少的路径如下：")
 	for i = 0; i < count; i++ {
 		if i == 0 {
-			fmt.Print(G.vex[mindist[i]])
+			fmt.Print(G.Vex[mindist[i]])
 		} else {
-			fmt.Printf("-->%s", G.vex[mindist[i]])
+			fmt.Printf("-->%s", G.Vex[mindist[i]])
 		}
 	}
 	fmt.Println("\n\n")
@@ -440,16 +440,16 @@ func displaySimplePath(G *AdjMatrix) {
 }
 func Dijkstra(G *AdjMatrix, start int, dist []int, path [][]int) {
 	var mindist, i, j, k, t int
-	for i = 1; i <= G.vexnum; i++ {
-		dist[i] = G.arcs[start][i]
-		if G.arcs[start][i] != INFINITY {
+	for i = 1; i <= G.Vexnum; i++ {
+		dist[i] = G.Arcs[start][i]
+		if G.Arcs[start][i] != INFINITY {
 			path[i][1] = start
 		}
 	}
 
-	for i = 2; i <= G.vexnum; i++ {
+	for i = 2; i <= G.Vexnum; i++ {
 		mindist = INFINITY
-		for j = 1; j <= G.vexnum; j++ {
+		for j = 1; j <= G.Vexnum; j++ {
 			if path[j][0] == 0 && dist[j] < mindist {
 				k = j
 				mindist = dist[j]
@@ -459,9 +459,21 @@ func Dijkstra(G *AdjMatrix, start int, dist []int, path [][]int) {
 			return
 		}
 		path[k][0] = 1
-		for j = 1; j <= G.vexnum; j++ {
-			if path[j][0] == 0 && G.arcs[k][j] < INFINITY && dist[k]+G.arcs[k][j] < dist[j] {
-				dist[j] = dist[k] + G.arcs[k][j]
+		//for j = 1; j <= G.Vexnum; j++ {
+		//	if path[j][0] == 0 && G.Arcs[k][j] < INFINITY && dist[k]+G.Arcs[k][j] < dist[j] {
+		//		dist[j] = dist[k] + G.Arcs[k][j]
+		//		t = 1
+		//		for path[k][t] != 0 {
+		//			path[j][t] = path[k][t]
+		//			t++
+		//		}
+		//		path[j][t] = k
+		//		path[j][t+1] = 0
+		//	}
+		//}
+		for j = 1; j <= G.Vexnum; j++ {
+			if path[j][0] == 0 && G.Arcs[k][j] < INFINITY && dist[k]+G.Arcs[k][j] < dist[j] {
+				dist[j] = dist[k] + G.Arcs[k][j]
 				t = 1
 				for path[k][t] != 0 {
 					path[j][t] = path[k][t]
@@ -471,16 +483,26 @@ func Dijkstra(G *AdjMatrix, start int, dist []int, path [][]int) {
 				path[j][t+1] = 0
 			}
 		}
+
+		// 更新路径信息
+		t = 1
+		for path[k][t] != 0 {
+			path[j][t] = path[k][t]
+			t++
+		}
+		path[j][t] = k
+		path[j][t+1] = 0
 	}
 }
 func displayShortestPath(G *AdjMatrix) {
+	//实现显示指定两地最短路径的逻辑
 	clearCommand := exec.Command("clear")
 	clearCommand.Stdout = os.Stdout
 	clearCommand.Run()
 
 	var str1, str2 string
 	var start, end int
-	dist := make([]int, MAXVEX)
+	dist := make([]int, MAXVex)
 	fmt.Print("请输入起点和终点，中间用空格隔开：")
 	fmt.Scan(&str1, &str2)
 
@@ -493,39 +515,40 @@ func displayShortestPath(G *AdjMatrix) {
 		return
 	}
 
-	path := make([][]int, MAXVEX)
-	for i := 0; i < MAXVEX; i++ {
-		path[i] = make([]int, MAXVEX)
+	path := make([][]int, MAXVex)
+	for i := 0; i < MAXVex; i++ {
+		path[i] = make([]int, MAXVex)
 	}
 
 	Dijkstra(G, start, dist, path)
 
 	i := 1
 	for path[end][i] != 0 {
-		fmt.Printf("%s-->", G.vex[path[end][i]])
+		fmt.Printf("%s-->", G.Vex[path[end][i]])
 		i++
 	}
-	fmt.Println(G.vex[end])
+	fmt.Println(G.Vex[end])
 
 	fmt.Scanln()
 	fmt.Scanln()
 }
 func addNewAddressPath(G *AdjMatrix) {
+	//实现增加新地点和路径的逻辑
 	var str string
-	var vexnum, num1, i, distance int
+	var Vexnum, num1, i, distance int
 	fmt.Print("请输入地点名：")
 	fmt.Scan(&str)
 
-	vexnum = G.vexnum
-	G.vexnum++
+	Vexnum = G.Vexnum
+	G.Vexnum++
 	fmt.Print("请输入地点的介绍：")
-	fmt.Scan(&G.info[G.vexnum])
+	fmt.Scan(&G.Info[G.Vexnum])
 
-	vexnum++
-	G.vex[vexnum] = str
-	for i = 1; i <= G.vexnum; i++ {
-		G.arcs[vexnum][i] = INFINITY
-		G.arcs[i][vexnum] = INFINITY
+	Vexnum++
+	G.Vex[Vexnum] = str
+	for i = 1; i <= G.Vexnum; i++ {
+		G.Arcs[Vexnum][i] = INFINITY
+		G.Arcs[i][Vexnum] = INFINITY
 	}
 
 	fmt.Println("请输入与该地点有路径的地点和它们之间的距离（中间用空格隔开，0 0 代表结束输入）：")
@@ -541,14 +564,15 @@ func addNewAddressPath(G *AdjMatrix) {
 			fmt.Scanln()
 			return
 		} else {
-			G.arcs[vexnum][num1] = distance
-			G.arcs[num1][vexnum] = distance
+			G.Arcs[Vexnum][num1] = distance
+			G.Arcs[num1][Vexnum] = distance
 		}
 	}
 
-	WriteFileAdjMatrix(G)
+	WriteFileAdjMatrix("AdjMatrix.txt", G)
 }
 func delOldAddress(G *AdjMatrix) {
+	//实现删除旧地点的逻辑
 	var str string
 	fmt.Print("输入要删除的地点：")
 	fmt.Scan(&str)
@@ -562,26 +586,27 @@ func delOldAddress(G *AdjMatrix) {
 	}
 
 	var i, j int
-	for i = num; i <= G.vexnum-1; i++ {
-		G.vex[i] = G.vex[i+1]
-		G.info[i] = G.info[i+1]
+	for i = num; i <= G.Vexnum-1; i++ {
+		G.Vex[i] = G.Vex[i+1]
+		G.Info[i] = G.Info[i+1]
 	}
 
-	for i = num; i <= G.vexnum-1; i++ {
-		for j = 1; j <= G.vexnum; j++ {
-			G.arcs[i][j] = G.arcs[i+1][j]
+	for i = num; i <= G.Vexnum-1; i++ {
+		for j = 1; j <= G.Vexnum; j++ {
+			G.Arcs[i][j] = G.Arcs[i+1][j]
 		}
 	}
 
-	for j = num; j <= G.vexnum-1; j++ {
-		for i = 1; i <= G.vexnum-1; i++ {
-			G.arcs[i][j] = G.arcs[i][j+1]
+	for j = num; j <= G.Vexnum-1; j++ {
+		for i = 1; i <= G.Vexnum-1; i++ {
+			G.Arcs[i][j] = G.Arcs[i][j+1]
 		}
 	}
 
-	G.vexnum--
+	G.Vexnum--
 }
 func delOldPath(G *AdjMatrix) {
+	//实现删除指定路线的逻辑
 	clearCommand := exec.Command("clear")
 	clearCommand.Stdout = os.Stdout
 	clearCommand.Run()
@@ -603,29 +628,29 @@ func delOldPath(G *AdjMatrix) {
 		fmt.Scanln()
 		fmt.Scanln()
 		return
-	} else if G.arcs[num1][num2] == INFINITY {
+	} else if G.Arcs[num1][num2] == INFINITY {
 		fmt.Println("该路径不存在！任意键返回")
 		fmt.Scanln()
 		fmt.Scanln()
 		return
 	} else {
-		G.arcs[num1][num2] = INFINITY
+		G.Arcs[num1][num2] = INFINITY
 	}
 
 	return
 }
 func addInfo(G *AdjMatrix) {
 	var i int
-	for i = 1; i <= G.vexnum; i++ {
-		fmt.Printf("请输入%s的介绍：", G.vex[i])
-		fmt.Scan(&G.info[i])
+	for i = 1; i <= G.Vexnum; i++ {
+		fmt.Printf("请输入%s的介绍：", G.Vex[i])
+		fmt.Scan(&G.Info[i])
 	}
 
-	WriteFileAdjMatrix(G)
+	WriteFileAdjMatrix("AdjMatrix.txt", G)
 }
 
 func main() {
 	G := new(AdjMatrix)
-	ReadFileCreateAdjMatrix(G)
+	ReadFileCreateAdjMatrix("AdjMatrix.txt", G)
 	menu(G)
 }
